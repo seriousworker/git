7a3c158d0f38a14c7eb821bcf19281f0dd41cbfa
Roman <roman4ik19871987@gmail.com>
2022-06-17 19:52:40 +0300

    Task from the codewars Simple validation of a username with regex

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
-function simpleMultiplication(number) {
-    return (number % 2 == 0) ? number * 8 : number * 9;
+function validateUsr(username) {
+    if (username.length >= 4 && username.length <= 16) {
+        let value = /^[a-z0-9_]+$/.exec(username);
+        let res = !!value ? username : null;
+        return res;
+    } else return false;
 }
 
-alert(simpleMultiplication(3));
+alert(validateUsr('p1pp1'));
+alert(validateUsr('DS3@DFx'));
