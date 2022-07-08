45fa5ee307e38f575873f953fbe2a6822ee2596a
Roman <roman4ik19871987@gmail.com>
2022-07-08 19:25:47 +0300

    test

--- newTest.js
+++ newTest.js
@@ -1,11 +1,12 @@
-let teem = {'tim':0, 'jim':2, 'randy':0, 'sandy':7, 'andy':0, 'katie':5, 'laura':1, 'saajid':2, 'alex':3, 'john':2, 'mr':0};
+let teem = {"tim":4,"jim":2,"randy":6,"sandy":4,"andy":8,"katie":7,"laura":3,"saajid":5,"alex":7,"john":5,"mr":2};
 
 function getMood(teem, names) {
     for (let name of Object.keys(teem)) {
         if (name == names) {
-            teem.name <= 5 ? alert('Get Out Now') : alert('Nice Work Champ!');
+            teem[name] < 5 ? alert('Get Out Now!') : alert('Nice Work Champ!');
         }
     }
 }
 
-getMood(teem, 'randy');
+
+getMood(teem, 'john');
