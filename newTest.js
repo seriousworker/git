7372f9732fe7b2d65923605200e447b777e941a9
Roman <roman4ik19871987@gmail.com>
2022-07-09 22:01:41 +0300

    commit with Char Code Calculation

--- newTest.js
+++ newTest.js
@@ -1,12 +1,39 @@
-let teem = {"tim":4,"jim":2,"randy":6,"sandy":4,"andy":8,"katie":7,"laura":3,"saajid":5,"alex":7,"john":5,"mr":2};
+function calc(x) {
+    let arrayOfNumbers = [];
 
-function getMood(teem, names) {
-    for (let name of Object.keys(teem)) {
-        if (name == names) {
-            teem[name] < 5 ? alert('Get Out Now!') : alert('Nice Work Champ!');
-        }
+    for (let char of x) {
+        arrayOfNumbers.push(char.charCodeAt());
     }
+
+    let temp = arrayOfNumbers.join('');
+    let temp2 = [...temp];
+    let sumOfNumbersBefore = 0;
+
+    for (let i of temp2) {
+        sumOfNumbersBefore += +i;
+    }
+
+    for (let i of temp2) {
+      if (i === '7') {
+        temp2[temp2.indexOf(i)] = '1'
+      }
+    }
+
+    let temp3 = temp2.join('');
+    let temp4 = [...temp3];
+    let sumOfNumbersAfter = 0;
+
+    for (let i of temp4) {
+        sumOfNumbersAfter += +i;
+    }
+
+    let result = sumOfNumbersBefore - sumOfNumbersAfter;
+
+    return  result;
 }
 
-
-getMood(teem, 'john');
+alert(calc('abcdef'));
+alert(calc('ifkhchlhfd'));
+alert(calc('aaaaaddddr'));
+alert(calc('jfmgklf8hglbe'));
+alert(calc('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'));
