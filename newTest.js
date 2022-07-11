4f34ad692a69369700ea0d5f90fe238efec205bb
Roman <roman4ik19871987@gmail.com>
2022-07-11 17:30:35 +0300

    Last solutions

--- newTest.js
+++ newTest.js
@@ -1,36 +1,4 @@
-function calc(x) {
-    let arrayOfNumbers = [];
-
-    for (let char of x) {
-        arrayOfNumbers.push(char.charCodeAt());
-    }
-
-    let temp = arrayOfNumbers.join('');
-    let temp2 = [...temp];
-    let sumOfNumbersBefore = 0;
-
-    for (let i of temp2) {
-        sumOfNumbersBefore += +i;
-    }
-
-    for (let i of temp2) {
-      if (i === '7') {
-        temp2[temp2.indexOf(i)] = '1'
-      }
-    }
-
-    let temp3 = temp2.join('');
-    let temp4 = [...temp3];
-    let sumOfNumbersAfter = 0;
-
-    for (let i of temp4) {
-        sumOfNumbersAfter += +i;
-    }
-
-    let result = sumOfNumbersBefore - sumOfNumbersAfter;
-
-    return  result;
-}
+const calc=x=>(x.replace(/./g,x=>x.charCodeAt()).match(/7/g)||[]).length*6
 
 alert(calc('abcdef'));
 alert(calc('ifkhchlhfd'));
